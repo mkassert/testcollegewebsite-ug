c4fad2a8aac2ec0c3121936dc7f717579b6bc681
Manish Kesharwani <manish@assertit.io>
2023-03-10 08:50:02 +0000

    Update MyCard.js

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -6,12 +6,12 @@ import {
   Card,
   Row,
   Col,
-  Carousel
+  Carousel,
 } from "react-bootstrap";
 
 export default function MyCard() {
   return (
-    <Container fluid style={{ position: "relative"}}>
+    <Container fluid style={{ position: "relative" }}>
       <Container>
         <Row className="p-2">
           <Col lg={4} sm={12} className=" ">
@@ -26,7 +26,16 @@ export default function MyCard() {
             <Form>
               <Container fluid>
                 <Row>
-                  <Col xs={12} lg={12}>
+                  <Col xs={6} lg={6}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" />
+                      <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                      </Form.Text>
+                    </Form.Group>
+                  </Col>
+                  <Col xs={6} lg={6}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                       <Form.Label>Email address</Form.Label>
                       <Form.Control type="email" placeholder="Enter email" />
@@ -37,7 +46,7 @@ export default function MyCard() {
                   </Col>
                 </Row>
                 <Row>
-                  <Col>
+                  <Col xs={6} lg={6}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                       <Form.Label>Email address</Form.Label>
                       <Form.Control type="email" placeholder="Enter email" />
@@ -46,7 +55,7 @@ export default function MyCard() {
                       </Form.Text>
                     </Form.Group>
                   </Col>
-                  <Col>
+                  <Col xs={6} lg={6}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                       <Form.Label>Email address</Form.Label>
                       <Form.Control type="email" placeholder="Enter email" />
@@ -57,15 +66,76 @@ export default function MyCard() {
                   </Col>
                 </Row>
                 <Row>
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                  <Col xs={6} lg={6}>
+                    <Form.Select aria-label="Default select example">
+                      <option>Select Courses</option>
+                      <option value="BBA">BBA</option>
+                      <option value="2">BCA</option>
+                      <option value="3">B.COM</option>
+                      <option value="3">MBA</option>
+                      <option value="3">MCA</option>
+                      <option value="3">B.A.LLB</option>
+
+                      <option value="3">B.COM</option>
+                      <option value="3">LLB</option>
+                      <option value="3">B.COM LLB</option>
+                    </Form.Select>
+                  </Col>
+                  <Col xs={6} lg={6}>
+                    <Form.Select aria-label="Default select example">
+                      <option value="">Select state</option>
+                      <option value="AN">Andaman and Nicobar Islands</option>
+                      <option value="AP">Andhra Pradesh</option>
+                      <option value="AR">Arunachal Pradesh</option>
+                      <option value="AS">Assam</option>
+                      <option value="BR">Bihar</option>
+                      <option value="CH">Chandigarh</option>
+                      <option value="CT">Chhattisgarh</option>
+                      <option value="DN">Dadra and Nagar Haveli</option>
+                      <option value="DD">Daman and Diu</option>
+                      <option value="DL">Delhi</option>
+                      <option value="GA">Goa</option>
+                      <option value="GJ">Gujarat</option>
+                      <option value="HR">Haryana</option>
+                      <option value="HP">Himachal Pradesh</option>
+                      <option value="JK">Jammu and Kashmir</option>
+                      <option value="JH">Jharkhand</option>
+                      <option value="KA">Karnataka</option>
+                      <option value="KL">Kerala</option>
+                      <option value="LA">Ladakh</option>
+                      <option value="LD">Lakshadweep</option>
+                      <option value="MP">Madhya Pradesh</option>
+                      <option value="MH">Maharashtra</option>
+                      <option value="MN">Manipur</option>
+                      <option value="ML">Meghalaya</option>
+                      <option value="MZ">Mizoram</option>
+                      <option value="NL">Nagaland</option>
+                      <option value="OR">Odisha</option>
+                      <option value="PY">Puducherry</option>
+                      <option value="PB">Punjab</option>
+                      <option value="RJ">Rajasthan</option>
+                      <option value="SK">Sikkim</option>
+                      <option value="TN">Tamil Nadu</option>
+                      <option value="TG">Telangana</option>
+                      <option value="TR">Tripura</option>
+                      <option value="UP">Uttar Pradesh</option>
+                      <option value="UT">Uttarakhand</option>
+                      <option value="WB">West Bengal</option>
+                    </Form.Select>
+                  </Col>
+                </Row>
+                <Row>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                  >
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
                   </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                  </Form.Group>
+                </Row>
+                <Row>
                   <Button variant="primary" type="submit">
-                    Submit
+                    Send Message
                   </Button>
                 </Row>
               </Container>
